0b36bfc058f5da0fa46c6e0cfdf85c31cb905a62
smpatric <patrick.cody.stewart@gmail.com>
2015-03-27 22:46:11 +0000

    Added Bootstrap to Idea Submission page, Added AJAX for Idea tracking

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -1,4 +1,33 @@
 # Homepage (Root path)
+require "sinatra/json"
+
 get '/' do
   erb :index
+end
+
+get '/dashboard' do
+  erb:'forms/dashboard'
+end
+
+post '/dashboard' do
+  title = params[:title]
+  description = params[:description]
+
+  @idea = Idea.new(
+    title: params[:title],
+    description: params[:description]
+    )
+
+  if @idea.save
+    json @idea
+  end
+  # if @idea.save
+  #   redirect '/'
+  # else
+  #   erb:'forms/dashboard'
+  # end
+end
+
+post '/vote' do
+  
 end
--- sinatra-skeleton/app/models/idea.rb
+++ sinatra-skeleton/app/models/idea.rb
@@ -1,4 +1,4 @@
 class Idea < ActiveRecord::Base
   has_many :students
-  validates :author, :title, :description, presence: true
+  validates :title, :description, presence: true
 end
