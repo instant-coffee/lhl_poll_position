5e506f6ad63c56e457d42626eaf4528f85c4d1ab
smpatric <patrick.cody.stewart@gmail.com>
2015-03-28 22:18:06 +0000

    Adjusted routes, added admin page, Connected voting page to db and introduced vote count JS functionality

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -6,29 +6,35 @@ get '/' do
 end
 
 get '/submit' do
-  erb:'submit'
+  erb:'user/submit'
+end
+
+get '/admin' do
+  erb:'admin/manage_ideas'
 end
 
 get '/vote' do
+  @ideas = Idea.all
   erb:'/vote'
 end
 
 post '/submit' do
   title = params[:title]
   description = params[:description]
+  
+  # @student = Student.new(
+  #   name: params[:author]
+  #   )
 
   @idea = Idea.new(
+    author: params[:author],
     title: params[:title],
     description: params[:description]
     )
 
   if @idea.save
     json @idea
+    # json @student
   end
-  # if @idea.save
-  #   redirect '/'
-  # else
-  #   erb:'forms/dashboard'
-  # end
 end
 
