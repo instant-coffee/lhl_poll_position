36dd5dcb3428108bf6da011eb829e35753f87011
smpatric <patrick.cody.stewart@gmail.com>
2015-03-29 19:41:17 +0000

    Latest push

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -5,12 +5,9 @@ get '/' do
   erb :index
 end
 
+# # # Student Pages # # #
 get '/submit' do
   erb:'user/submit'
-end
-
-get '/admin' do
-  erb:'admin/manage_ideas'
 end
 
 get '/vote' do
@@ -18,13 +15,25 @@ get '/vote' do
   erb:'user/vote'
 end
 
-post '/submit' do
-  title = params[:title]
-  description = params[:description]
-  
-  # @student = Student.new(
-  #   name: params[:author]
-  #   )
+get '/about' do
+  erb:'about'
+end
+
+# # # Admin Pages # # #
+get '/admin/ideas' do
+  @ideas = Idea.all
+  erb:'admin/manage_ideas'
+end
+
+get '/admin/groups' do
+  @ideas = Idea.all
+  erb:'admin/manage_groups'
+end
+
+
+
+# Add idea to Ideas Table
+post '/ideas' do
 
   @idea = Idea.new(
     author: params[:author],
@@ -34,7 +43,61 @@ post '/submit' do
 
   if @idea.save
     json @idea
-    # json @student
   end
 end
+
+# Add student to Students table
+post '/students' do
+
+  @student = Student.new(
+    name: params[:author]
+    )
+
+  if @student.save
+    json @student
+  end
+end
+
+# # # Admin: Manage ideas # # #
+#Retrieve idea to edit
+get '/edit_idea/:id' do
+  @idea = Idea.find(params[:id])
+  erb:'admin/edit_idea'
+end
+
+# Update idea after edit
+post '/update_idea/:id' do
+  idea = Idea.find(params[:id])
+  idea.title = params[:title]
+  idea.description = params[:description]
+  idea.save
+
+  redirect 'admin/ideas'
+end
+
+# Toggle True/False for inclusion in final Ideas list
+get '/add_final/:id' do
+ idea = Idea.find(params[:id])
+ if idea.final == false
+  idea.final = true
+  idea.save
+ else
+  idea.final = false
+  idea.save
+ end
+
+ redirect 'admin/ideas'
+end
+
+# Delete idea from list
+get '/delete_idea/:id' do
+  Idea.find(params[:id]).destroy
+
+  redirect 'admin/ideas'
+end
+
+
+
+
+
 
--- sinatra-skeleton/db/schema.rb
+++ sinatra-skeleton/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326211219) do
+ActiveRecord::Schema.define(version: 20150329040842) do
 
   create_table "admins", force: true do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150326211219) do
     t.string  "title"
     t.string  "description"
     t.integer "votes"
+    t.boolean "final",       default: false
   end
 
   create_table "students", force: true do |t|
