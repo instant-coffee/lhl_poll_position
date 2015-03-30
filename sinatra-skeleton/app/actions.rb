1822218fa9c291202c0c051deabd5df8fc134748
smpatric <patrick.cody.stewart@gmail.com>
2015-03-30 05:04:11 +0000

    Working update

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -1,40 +1,26 @@
 # Homepage (Root path)
 require "sinatra/json"
+require_relative 'admin_actions'
+require_relative 'student_actions'
 
 get '/' do
   erb :index
 end
 
 # # # Student Pages # # #
-get '/submit' do
-  erb:'user/submit'
+get '/student/submit' do
+  erb:'student/submit'
 end
 
-get '/vote' do
+get '/student/vote' do
   @ideas = Idea.all
-  erb:'user/vote'
+  erb:'student/vote'
 end
 
-get '/about' do
-  erb:'about'
-end
-
-post '/cast' do
- params[:votes].each do |key, val|
-   idea =  Idea.find(key)
-   if val == "on"
-    idea.votes += 1
-    idea.save
-   end
- end
- redirect '/results'
-end
-
-get '/results' do
+get '/student/results' do
   @ideas = Idea.where("votes > '0'")
-  erb:'user/result'
+  erb:'student/results'
 end
-
 
 # # # Admin Pages # # #
 get '/admin/ideas' do
@@ -42,105 +28,35 @@ get '/admin/ideas' do
   erb:'admin/manage_ideas'
 end
 
-get '/admin/groups' do
-  @ideas = Idea.all
+#Retrieve idea to edit
+get '/update_idea/:id' do
+  @idea = Idea.find(params[:id])
+  erb:'admin/update_idea'
+end
+
+get '/admin/manage_groups' do
+  @ideas = Idea.where("votes > 0").order(votes: :desc)
+  @students = Student.all
   erb:'admin/manage_groups'
 end
 
-
-
-# Add idea to Ideas Table
-post '/ideas' do
-
-  @idea = Idea.new(
-    author: params[:author],
-    title: params[:title],
-    description: params[:description]
-    )
-
-  if @idea.save
-    json @idea
-  end
-end
-
-# Add student to Students table
-post '/students' do
-
-  @student = Student.new(
-    name: params[:author]
-    )
-
-  if @student.save
-    json @student
-  end
-end
-
-# # # Admin: Manage ideas # # #
-#Retrieve idea to edit
-get '/edit_idea/:id' do
-  @idea = Idea.find(params[:id])
-  erb:'admin/edit_idea'
-end
-
-# Update idea after edit
-post '/update_idea/:id' do
-  idea = Idea.find(params[:id])
-  idea.title = params[:title]
-  idea.description = params[:description]
-  idea.save
-
-  redirect 'admin/ideas'
-end
-
-# Toggle True/False for inclusion in final Ideas list
-get '/add_final/:id' do
- idea = Idea.find(params[:id])
- if idea.final == false
-  idea.final = true
-  idea.save
- else
-  idea.final = false
-  idea.save
- end
-
- redirect 'admin/ideas'
-end
-
-# Delete idea from list
-get '/delete_idea/:id' do
-  Idea.find(params[:id]).destroy
-
-  redirect 'admin/ideas'
-end
-
-### Priya
-
-get '/admin/manage_ideas_test' do
- @ideas = Idea.where("votes > 0").order(votes: :desc)
- @students = Student.all
- erb:'admin/manage_ideas_test'
-end
-
-
-post '/admin/manage_ideas_test' do
-  params.each do |k,v| 
-   student= Student.find(k) 
-    student.idea_id=v[0] 
-    student.save
-  end
-  redirect '/admin/final_groups'
-
-end
-
-Priya Thanya [5:16 PM]
-get '/admin/final_groups' do
+get '/admin/groups' do
   @ideas = Idea.where("votes > 0").order(votes: :desc).limit(6)
-
-erb:'admin/final_groups'
+  @students = Student.all
+erb:'/admin/final_groups'
 end
 
+get '/whoareyou' do
+  erb:'whoareyou'
+end
 
+get '/about' do
+  erb:'about'
+end
 
-
-
+get '/admin/final' do
+  @ideas = Idea.where("votes > 0").order(votes: :desc)
+  @students = Student.all
+  erb:'admin/final'
+end
 
