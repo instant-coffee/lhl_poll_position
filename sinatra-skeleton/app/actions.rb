e24231ec3be6a33e270cbd3c5876b8a59f4713fd
katchy <priya.thanya@gmail.com>
2015-03-30 00:07:30 +0000

    first

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -1,8 +1,38 @@
 # Homepage (Root path)
 require "sinatra/json"
 
+get '/admin/manage_ideas_test' do
+  @ideas = Idea.where("votes > 0").order(votes: :desc)
+  @students = Student.all
+  erb:'admin/manage_ideas_test'
+end
+
+
+
+post '/admin/manage_ideas_test' do
+   params.each do |k,v| 
+    student= Student.find(k) 
+     student.idea_id=v[0] 
+     student.save
+   end
+   redirect '/admin/final_groups'
+ 
+end
+
+get '/admin/final_groups' do
+   @ideas = Idea.where("votes > 0").order(votes: :desc).limit(6)
+
+ erb:'admin/final_groups'
+end
+  
+  
+
 get '/' do
   erb :index
+end
+
+get '/login' do
+  erb :login
 end
 
 get '/submit' do
@@ -36,5 +66,9 @@ post '/submit' do
     json @idea
     # json @student
   end
+end
+
+get '/admin/manage_ideas_test' do
+  erb:'admin/manage_ideas_test'
 end
 
